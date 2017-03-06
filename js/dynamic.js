cdc8b1577e2d355ef796e5a65d2de866efc2e77a
simar <simarpreet@knoldus.in>
2017-03-06 21:29:32 +0530

    Test Submission by Simar

---
+++ js/dynamic.js
@@ -0,0 +1,10 @@
+
+
+function hideOrShow() {
+    var x = document.getElementById('hideOrShow');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
